554673e0453a0f6255f5b1f0c6348c510738a53a
Allen Li <love22798800@gmail.com>
2023-09-27 22:47:48 +0800

    重構資料處理流程for test data

--- F1365_sample_code/ch04-model-interface/code/run.py
+++ F1365_sample_code/ch04-model-interface/code/run.py
@@ -4,6 +4,7 @@ import pandas as pd
 from model_nn import ModelNN
 from model_xgb import ModelXGB
 from runner import Runner
+from rerunner import ReRunner
 from util import Submission
 
 if __name__ == '__main__':
@@ -38,11 +39,25 @@ if __name__ == '__main__':
     # 設定特徵的名字
     features = [f'feat_{i}' for i in range(1, 94)]
 
+    def load_x_train() -> pd.DataFrame:
+        return pd.read_csv('../input/train.csv').iloc[:, :-1]
+
+    def load_y_train() -> pd.Series:
+        train_y = pd.read_csv('../input/train.csv')['target']
+        train_y = np.array([int(st[-1]) for st in train_y]) - 1
+        train_y = pd.Series(train_y)
+        return train_y
+
+    def load_x_test() -> pd.DataFrame:
+        return pd.read_csv('../input/test.csv')[features[:2]]
+    tr_x, tr_y, va_x = load_x_train(), load_y_train(), load_x_test()
+
     # # 訓練 xgboost 模型，並做出預測
-    # runner = Runner('xgb1', ModelXGB, features, params_xgb)
-    # runner.run_train_cv()
-    # runner.run_predict_cv()
-    # Submission.create_submission('xgb1')
+    runner = ReRunner('xgb2', ModelXGB, features, params_xgb, tr_x, tr_y)
+    runner.run_train_cv()
+    runner = ReRunner('xgb2', ModelXGB, features, params_xgb, va_x, 'na')
+    runner.run_predict_cv()
+    Submission.create_submission('xgb2')
     #
     # # 訓練類神經網路，並做出預測
     # runner = Runner('nn1', ModelNN, features, params_nn)
@@ -57,7 +72,8 @@ if __name__ == '__main__':
     runner.run_test_all()
     Submission.create_submission('xgb1-train-all')
     '''
-    runner = Runner('xgb1-train-all', ModelXGB, features, params_xgb_all)
-    runner.run_train_all()
-    runner.run_predict_all()
-    Submission.create_submission('xgb1-train-all')
+    # runner = ReRunner('xgb1-train-all', ModelXGB, features, params_xgb_all, tr_x, tr_y)
+    # runner.run_train_all()
+    # runner = ReRunner('xgb1-train-all', ModelXGB, features, params_xgb_all, va_x, 'na')
+    # runner.run_predict_all()
+    # Submission.create_submission('xgb1-train-all')
